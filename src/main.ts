2b58af8f04e4b1eb9d0ec274c0955d9240ebe5b9
Hudson Oliveira Pereira <hudson.o.pereira@gmail.com>
2025-02-26 07:24:49 -0300

    added hbs engine initial configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './module/app.module';
+import { join } from 'path';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // await app.listen(process.env.PORT ?? 3000);
+
+  app.useStaticAssets(join(__dirname, `..`, `public`));
+  app.setBaseViewsDir(join(__dirname, `..`, `views`));
+  app.setViewEngine(`hbs`);
+
   app.listen(process.env.PORT ?? 3000, () => {
     console.log(`Rodando em http://localhost:${port}.`);
   });
