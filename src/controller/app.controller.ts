6678bad3c848173d41c50eaef9017b5491694731
Hudson Oliveira Pereira <hudson.o.pereira@gmail.com>
2025-02-26 23:39:06 -0300

    feat: altered render method for dinamic method

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -1,19 +1,15 @@
-import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AppService } from '../service/app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-
-  @Get() 
-  @Render(`index`)
-  root() {
-    return { message: `Hello World! okkk` };
+  @Get()
+  root(@Res() res: Response) {
+    return res.render(
+      this.appService.getViewName(),
+      { message: 'Hello world! MVC' },
+    );
   }
 }
