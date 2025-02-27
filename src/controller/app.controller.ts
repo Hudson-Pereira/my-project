a52bfdd92273c920870c5489a607e46c5b5b4565
Hudson Oliveira Pereira <hudson.o.pereira@gmail.com>
2025-02-26 23:33:22 -0300

    fix: path for MVC method

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from '../service/app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
-  @Get()
+  @Get() 
   @Render(`index`)
   root() {
-    return { message: `Hello World!` };
+    return { message: `Hello World! okkk` };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './module/app.module';
-import { join } from 'path';
+import { resolve } from 'path';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); //config for MVC method
   // await app.listen(process.env.PORT ?? 3000);
 
-  app.useStaticAssets(join(__dirname, `..`, `public`));
-  app.setBaseViewsDir(join(__dirname, `..`, `views`));
-  app.setViewEngine(`hbs`);
+  app.useStaticAssets(resolve(`./src/public`)); //path for looking for public itens
+  app.setBaseViewsDir(resolve(`./src/views`)); //path for looking for views
+  app.setViewEngine(`hbs`); //set the engine for render pages
 
   app.listen(process.env.PORT ?? 3000, () => {
     console.log(`Rodando em http://localhost:${port}.`);
